18de68941310989bf627ddb0a321c967164ea0bf
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-01-25 11:51:27 +0300

    New

--- graphfusionai/tools/standard/email_tool.py
+++ graphfusionai/tools/standard/email_tool.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
